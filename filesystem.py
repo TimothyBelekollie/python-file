84662e6ba7181f3c69dfa47cbd7706c50a5ed1db
TimothyBelekollie <belekollietimothy2@gmail.com>
2023-06-20 12:04:49 +0200

    des: file manipulation in python

---
+++ filesystem.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 20 11:34:27 2023
+
+@author: Timothy Belekollie
+
+"""
+
+file = open("/Users/belek/Desktop/Data Science/File/words.txt")
+file.read()
+
